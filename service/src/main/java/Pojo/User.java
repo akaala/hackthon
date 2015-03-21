b117cb2791c7969ca355a2e3f9ab55abef080e4b
qinf <qinf@Ctrip.com>
2015-03-21 17:23:24 +0800

    user service

--- service/src/main/java/Pojo/User.java
+++ service/src/main/java/Pojo/User.java
@@ -4,4 +4,28 @@ public class User {
     int userid;
     String name;
     String sex;
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
